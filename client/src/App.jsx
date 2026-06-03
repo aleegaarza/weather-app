0f3adc73083f0c3569b4796104f12635741efa73
Ale Garza Olivas <alegarza@Ales-MacBook-Air.local>
2026-06-03 14:56:45 -0600

    peticion al backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,32 @@ import { useState } from 'react';
 
 function App() {
   const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log('Ciudad enviada:', city);
-  };
+    if (!city.trim()) {
+    setError('Please enter a city.');
+      return;
+    }
+    try {
+    setError("");
+    setLoading(true);
+    const response = await fetch(`http://localhost:3001/api/weather?city=${encodeURIComponent(city)}`);
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.error || 'Failed to fetch weather data.');
+    }
+    setWeatherData(data);
+  } catch (error) {
+    setError(error.message);
+    setWeatherData(null);
+  } finally {
+    setLoading(false);
+  }
+}
 
   return (
     <main>
@@ -17,10 +38,18 @@ function App() {
           type="text"
           placeholder="Enter a city"
           value={city}
-          onChange={(event) => setCity(event.target.value)}
+          onChange={(event) => {
+            setCity(event.target.value)
+            setError(null);
+          }}
         />
         <button type="submit">Search</button>
       </form>
+      <div>
+        {weatherData && <pre>{JSON.stringify(weatherData, null, 2)}</pre>}
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        {loading && <p>Loading...</p>}
+      </div>
     </main>
   );
 }
